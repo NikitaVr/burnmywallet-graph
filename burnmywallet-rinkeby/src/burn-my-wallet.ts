1766aee95e3e5d0b18006bce28bca2ed6fde9486
Nikita Varabei <nVarabei@gmail.com>
2022-06-25 12:08:53 -0400

    add graph for tracking tokens and users of burn my wallet

---
+++ burnmywallet-rinkeby/src/burn-my-wallet.ts
@@ -0,0 +1,27 @@
+import {
+  Transfer as TransferEvent,
+  BurnMyWallet as TokenContract,
+} from "../generated/BurnMyWallet/BurnMyWallet";
+
+import { Token, User } from "../generated/schema";
+
+export function handleTransfer(event: TransferEvent): void {
+  let token = Token.load(event.params.tokenId.toString());
+  if (!token) {
+    token = new Token(event.params.tokenId.toString());
+    token.creator = event.params.to.toHexString();
+    token.tokenID = event.params.tokenId;
+
+    let tokenContract = TokenContract.bind(event.address);
+    token.name = tokenContract.name();
+    token.createdAtTimestamp = event.block.timestamp;
+  }
+  token.owner = event.params.to.toHexString();
+  token.save();
+
+  let user = User.load(event.params.to.toHexString());
+  if (!user) {
+    user = new User(event.params.to.toHexString());
+    user.save();
+  }
+}
